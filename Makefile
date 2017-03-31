c8839ce897ac624fe3f3ec850c8de24de934312e
Leonidas <leonidasprado@yahoo.com>
2017-03-31 20:12:35 -0300

    First commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+blah: hello_world.cpp
+	g++ hello_world.cpp -o runme
+
+clean:
+	rm -f *o runme
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+cout << "Hello world!" << endl;
+cout << "Sua bicha!!!!" << endl;
+return 0;
+}
